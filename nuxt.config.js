badbf014a3dc2d26cbc9d872567d3371a715b47c
Sebastian Dittrich <sebastian.dittrich@live.de>
2019-12-08 21:07:44 +0100

    Changed Description

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,7 +9,6 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Die offizielle DKD-Merch Seite!' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
